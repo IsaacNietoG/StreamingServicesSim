3a8c9ea8a4c61c3584662021b6fd65bdbe5742fb
MrTaiChi <isaaniet@gmail.com>
2024-02-19 20:20:58 -0600

    doc: Aclaracion en Cliente

--- Cliente.java
+++ Cliente.java
@@ -3,7 +3,7 @@ public class Cliente implements Suscriptor{
     private String nombre;
     private Suscripcion[] suscripciones;
     private double cuentaBanco;
-    private Servicio listaServicios;
+    private Servicio listaServicios; //Esto es el enum jaja pero no se q variable
 
     
 }
