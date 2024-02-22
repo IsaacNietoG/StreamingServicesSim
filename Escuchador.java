f3d2f2ab3364a94f0c01dd1fa83cbb416a77705d
mrtaichi <isaaniet@gmail.com>
2024-02-21 21:04:14 -0600

    doc: Algunos cambios de nombres y correcciones de documentacion

---
+++ Escuchador.java
@@ -0,0 +1,17 @@
+/**
+   Interface para modelar el comportamiento básico que debe tener un Observer en nuestro patron.
+
+   Esta es la parte Observer de nuestra implementación del patrón Observer, la parte sujeto es
+   Notificador 
+ */
+public interface Escuchador {
+
+    /**
+       Este es el método que debe activar el Sujeto para enviar mensajes al Suscriptor
+    
+       @param   mensaje    Una String que recibe el Suscriptor, la cual contiene el texto que el
+                           Suscriptor recibe como mensaje.
+     */
+    public void recibirMensaje(String mensaje);
+
+}
